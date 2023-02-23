2e2b026ee2caf9d251a2b9325d758bdf680735aa
fabiosimoesds <fabio.santos@mmtm.io>
2023-02-23 08:52:36 +0000

    added sort by asc or desc + sort indicator

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,7 +4,7 @@ class CompaniesController < ApplicationController
   end
 
   def list
-    companies = Company.high_yield.order("#{params[:column]} asc")
+    companies = Company.high_yield.order("#{params[:column]} #{params[:direction]}")
     render(partial: 'companies/partials/companies', locals: { companies: companies })
   end
 end
--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,5 +1,17 @@
 module CompaniesHelper
   def sort_link(column:, label:)
-    link_to(label, list_companies_path(column: column), class: 'text-white text-decoration-none')
+    if column == params[:column]
+      link_to(label, list_companies_path(column: column, direction: change_direction), class: 'text-white text-decoration-none')
+    else
+      link_to(label, list_companies_path(column: column, direction: 'asc'), class: 'text-white text-decoration-none')
+    end
+  end
+
+  def change_direction
+    params[:direction] == 'asc' ? 'desc' : 'asc'
+  end
+
+  def sort_indicator
+    tag.span(class: "sort sort-#{params[:direction]}")
   end
 end
